cd483f79618c49b68480e3d3e66fb477308be8e3
Frangiskos <frangiskos@gmail.com>
2020-03-11 14:19:00 +0200

    feat: extend test coverage

--- src/tests.ts
+++ src/tests.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
-
-import * as assert from 'assert';
 import { sql, SqlConfig } from '.';
+import * as assert from 'assert';
+const AssertionError = assert.AssertionError;
 
 const sqlConfig: SqlConfig = {
     server: process.env.DB_SERVER!,
@@ -23,7 +23,17 @@ interface Person {
     isMarried: boolean;
 }
 
+const log = {
+    start(msg: string) {
+        console.log(`\nAsserting ${msg}...`);
+    },
+    end(msg: string) {
+        console.log(`Asserting ${msg} OK`);
+    }
+};
+
 async function runTests() {
+    log.start('sql.q create Table');
     await sql.q('DROP TABLE IF EXISTS people');
 
     await sql.q(`CREATE TABLE people (
@@ -33,8 +43,12 @@ async function runTests() {
         childrenCount int,
         salary money,
         isMarried bit
-    )
-    `);
+        )
+        `);
+
+    const tableId = await sql.queryValue(`SELECT OBJECT_ID (N'people', N'U')`);
+    assert(tableId !== null);
+    log.end('sql.q create Table');
 
     /** Test that the table exists */
     const tablePeople = await sql.q1(`SELECT OBJECT_ID('people', 'U')`);
@@ -48,7 +62,7 @@ async function runTests() {
         isMarried: true
     };
 
-    /** Insert data into DB */
+    log.start('sql.q Insert data into DB');
     await sql.q(
         `INSERT INTO people (name, birthdate, childrenCount, salary, isMarried) 
         VALUES (@P1, @P2, @P3, @P4, @P5)`,
@@ -58,35 +72,49 @@ async function runTests() {
         johnnyData.salary,
         johnnyData.isMarried
     );
+    assert(true);
+    log.end('sql.q Insert data into DB');
 
-    /** Retrieve first match from DB */
+    log.start('sql.q1 Retrieve first match from DB');
     const jonnyFromDB = await sql.q1(`SELECT * FROM people WHERE name = @P1`, 'Johnny');
     /** Test that the data retrieved are the same with the data inserted + id */
     assert.deepStrictEqual(jonnyFromDB, { id: 1, ...johnnyData });
     assert(typeof jonnyFromDB.id === 'number');
-    assert(typeof jonnyFromDB.name === 'string');
+    assert(jonnyFromDB.name === 'Johnny');
     assert(typeof jonnyFromDB.birthdate === 'object');
     assert((jonnyFromDB.birthdate as Date).toISOString() === '2000-01-01T00:00:00.000Z');
     assert(typeof jonnyFromDB.salary === 'number');
     assert(jonnyFromDB.salary === 2345.67);
     assert(typeof jonnyFromDB.isMarried === 'boolean');
+    log.end('sql.q1 Retrieve first match from DB');
 
+    log.start('sql.qv single value');
     /** Return value of the first key of the first record returned */
     const jonnyName = await sql.qv(`SELECT name FROM people WHERE name = @P1`, 'Johnny');
     assert(jonnyName === 'Johnny');
-    const noName = await sql.qv(`SELECT name FROM people WHERE name = @P1`, 'Nobody');
-    assert(noName === null);
+    log.end('sql.qv single value');
 
-    /** Add record and return identity */
+    log.start('sql.qv count');
+    const totalPersons = await sql.qv(`SELECT count(*) FROM people`);
+    assert(totalPersons === 1);
+    log.end('sql.qv count');
+
+    log.start('sql.qv Error');
+    //@ts-ignore
+    await assert.rejects(sql.qv(`SELECT name FROM people WHERE name = @P1`, 'Nobody'));
+    log.end('sql.qv Error');
+
+    log.start('sql.ii Insert and return identity');
     const id = await sql.ii(`INSERT INTO people (name) VALUES (@P1)`, 'Not Johnny');
     /** Test that identity is equal to 2 */
     assert.strictEqual(id, 2);
+    log.end('sql.ii Insert and return identity');
 
-    /** Get all records */
+    log.start('sql.q return recordset');
     const bothPersons = await sql.q('SELECT * FROM people');
-
     assert(Array.isArray(bothPersons));
     assert(bothPersons[1].birthdate === null);
+    log.end('sql.q return recordset');
 
     await sql.q('DROP TABLE IF EXISTS people');
 }
@@ -94,4 +122,12 @@ async function runTests() {
 runTests()
     .then(() => console.log('Tests completed'))
     .then(sql.close)
-    .catch(console.error);
+    .catch(e => {
+        if (e instanceof AssertionError) {
+            // Output expected AssertionErrors.
+            console.error(`Assertion failed.\n- Expected: ${e.expected}\n- Got: ${e.actual}\n-Error: ${e}`);
+        } else {
+            // Output unexpected Errors.
+            console.error('Unexpected error in tests\n', e);
+        }
+    });

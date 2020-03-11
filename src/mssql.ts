2296d1efa13d3e95f5d3d8b27b54461990f17204
Frangiskos <frangiskos@gmail.com>
2020-03-11 14:18:34 +0200

    refactor: use async/await

--- src/mssql.ts
+++ src/mssql.ts
@@ -16,6 +16,49 @@ export class SqlFactory {
         SqlFactory.instance = this;
     }
 
+    private async checkConnection() {
+        if (!this.pool) throw new Error('SQL not initialized. Use sql.init(config) first');
+
+        // // Already connected
+        if (this.pool.connected) {
+            this.timerReset();
+
+            // // Wait for connection
+        } else if (this.pool.connecting) {
+            // wait up to 10 sec to connect or reject
+            await new Promise((resolve, reject) => {
+                const handler = () => {
+                    if (this.pool && this.pool.connected) {
+                        clearInterval(waiting);
+                        clearTimeout(expireTimeout);
+                        this.timerReset();
+                        return resolve();
+                    }
+                };
+                const waiting = setInterval(handler, 100);
+                const expireTimeout = setTimeout(() => {
+                    clearInterval(waiting);
+                    reject('Is taking too long to connect to database');
+                }, 10000);
+            });
+
+            // // Connect
+        } else {
+            this.idleTimer = setTimeout(this.close, this.connectionTimeout);
+            await this.pool.connect().catch(error => {
+                // // pool.connect() error:
+                // ELOGIN (ConnectionError) - Login failed.
+                // ETIMEOUT (ConnectionError) - Connection timeout.
+                // EALREADYCONNECTED (ConnectionError) - Database is already connected!
+                // EALREADYCONNECTING (ConnectionError) - Already connecting to database!
+                // EINSTLOOKUP (ConnectionError) - Instance lookup failed.
+                // ESOCKET (ConnectionError) - Socket error.
+                console.error('SQL Connection Error: ', error);
+                throw error;
+            });
+        }
+    }
+
     public static getInstance(): SqlFactory {
         return SqlFactory.instance;
     }
@@ -30,100 +73,68 @@ export class SqlFactory {
     };
 
     /** Executes query and returns the result as an array of objects */
-    public query<T extends string | number | boolean | Date = any>(
+    public async query<T extends any = any>(
         sqlStr: string,
         ...params: Array<string | number | boolean | Date>
     ): Promise<mssql.IRecordSet<T>> {
+        // : Promise<T> {
+        // : Promise<mssql.IRecordSet<T>> {
         try {
-            return Promise.resolve()
-                .then(_ => {
-                    if (!this.pool) {
-                        throw 'SQL not initialized. Use sql.init(config) first';
-                    } else if (this.pool.connected) {
-                        this.timerReset();
-                        return this.pool;
-                    } else if (this.pool.connecting) {
-                        // wait up to 10 sec to connect
-                        return new Promise((resolve, reject) => {
-                            const handler = () => {
-                                if (this.pool && this.pool.connected) {
-                                    clearInterval(waiting);
-                                    this.timerReset();
-                                    return resolve(this.pool);
-                                }
-                            };
-                            const waiting = setInterval(handler, 100);
-                            setTimeout(() => {
-                                reject('Is taking too long to connect to database');
-                            }, 10000);
-                        });
-                    } else {
-                        this.idleTimer = setTimeout(this.close, this.connectionTimeout);
-                        return this.pool.connect().catch(error => {
-                            // // pool.connect() error:
-                            // ELOGIN (ConnectionError) - Login failed.
-                            // ETIMEOUT (ConnectionError) - Connection timeout.
-                            // EALREADYCONNECTED (ConnectionError) - Database is already connected!
-                            // EALREADYCONNECTING (ConnectionError) - Already connecting to database!
-                            // EINSTLOOKUP (ConnectionError) - Instance lookup failed.
-                            // ESOCKET (ConnectionError) - Socket error.
-                            console.error('SQL Connection Error: ', error);
-                            throw error;
-                        });
-                    }
-                })
-                .then(_ => new mssql.Request(this.pool))
-                .then(request => {
-                    let paramType:
-                        | mssql.ISqlTypeFactoryWithNoParams
-                        | mssql.ISqlTypeFactoryWithLength
-                        | mssql.ISqlTypeFactoryWithPrecisionScale;
-                    params.forEach((p, ix) => {
-                        switch (typeof p) {
-                            case 'string':
-                                paramType = mssql.NVarChar;
-                                break;
-                            case 'boolean':
-                                paramType = mssql.Bit;
-                                break;
-                            case 'number':
-                                if (Number.isInteger(p as number)) {
-                                    paramType = mssql.Int;
-                                } else {
-                                    paramType = mssql.Money;
-                                }
-                                break;
-                            case 'object': {
-                                if (p && Object.prototype.toString.call(p) === '[object Date]' && !isNaN(+p)) {
-                                    paramType = mssql.DateTime;
-                                    break;
-                                } else {
-                                    paramType = mssql.NVarChar;
-                                    break;
-                                }
-                            }
-                            default:
-                                paramType = mssql.NVarChar;
-                                break;
+            await this.checkConnection();
+
+            const request = new mssql.Request(this.pool);
+
+            let paramType:
+                | mssql.ISqlTypeFactoryWithNoParams
+                | mssql.ISqlTypeFactoryWithLength
+                | mssql.ISqlTypeFactoryWithPrecisionScale;
+
+            params.forEach((p, ix) => {
+                switch (typeof p) {
+                    case 'string':
+                        paramType = mssql.NVarChar;
+                        break;
+                    case 'boolean':
+                        paramType = mssql.Bit;
+                        break;
+                    case 'number':
+                        if (Number.isInteger(p as number)) {
+                            paramType = mssql.Int;
+                        } else {
+                            paramType = mssql.Money;
                         }
-                        request.input(`P${ix + 1}`, paramType, p);
-                    });
-                    const resultSet = request.query(sqlStr);
-                    return resultSet;
-                })
-                .then(resultSet => resultSet.recordset)
-                .catch(error => {
-                    // ETIMEOUT (RequestError) - Request timeout.
-                    // EREQUEST (RequestError) - Message from SQL Server
-                    // ECANCEL (RequestError) - Cancelled.
-                    // ENOCONN (RequestError) - No connection is specified for that request.
-                    // ENOTOPEN (ConnectionError) - Connection not yet open.
-                    // ECONNCLOSED (ConnectionError) - Connection is closed.
-                    // ENOTBEGUN (TransactionError) - Transaction has not begun.
-                    // EABORT (TransactionError) - Transaction was aborted (by user or because of an error).
-                    console.error('SQL Query Error: ', error);
-                    throw error;
-                });
+                        break;
+                    case 'object': {
+                        if (p && Object.prototype.toString.call(p) === '[object Date]' && !isNaN(+p)) {
+                            paramType = mssql.DateTime;
+                            break;
+                        } else {
+                            paramType = mssql.NVarChar;
+                            break;
+                        }
+                    }
+                    default:
+                        paramType = mssql.NVarChar;
+                        break;
+                }
+                request.input(`P${ix + 1}`, paramType, p);
+            });
+
+            try {
+                const resultSet = await request.query(sqlStr);
+                return resultSet.recordset;
+            } catch (error) {
+                // ETIMEOUT (RequestError) - Request timeout.
+                // EREQUEST (RequestError) - Message from SQL Server
+                // ECANCEL (RequestError) - Cancelled.
+                // ENOCONN (RequestError) - No connection is specified for that request.
+                // ENOTOPEN (ConnectionError) - Connection not yet open.
+                // ECONNCLOSED (ConnectionError) - Connection is closed.
+                // ENOTBEGUN (TransactionError) - Transaction has not begun.
+                // EABORT (TransactionError) - Transaction was aborted (by user or because of an error).
+                console.error('SQL Query Error: ', error);
+                throw error;
+            }
         } catch (error) {
             console.error('SQL Execution Error: ', error);
             throw error;
@@ -131,7 +142,7 @@ export class SqlFactory {
     }
 
     /** Executes the query and returns the first record (object) or null if no records found */
-    public async queryOne<T extends string | number | boolean | Date = any>(
+    public async queryOne<T extends {} = any>(
         sqlStr: string,
         ...params: Array<string | number | boolean>
     ): Promise<T | null> {
@@ -144,19 +155,22 @@ export class SqlFactory {
     }
 
     /**
-     * Executes the query and returns the first value of the first record or null if no records found
+     * Executes the query and returns the first value of the first record
+     * An error is thrown if no records found
      * Can be useful in cases like "SELECT COUNT (*) FROM Users" or "SELECT Name From Users WHERE id = @P1"
      */
-    public async queryValue<T extends string | number | boolean | Date = any>(
+    public async queryValue<T extends string | number | boolean | Date | null = any>(
         sqlStr: string,
         ...params: Array<string | number | boolean | Date>
-    ): Promise<T | null> {
+    ): Promise<T> {
         const recordset = await this.query(sqlStr, ...params);
 
         if (recordset.length && Object.keys(recordset[0]).length) {
             return recordset[0][Object.keys(recordset[0])[0]];
         } else {
-            return Promise.resolve(null);
+            throw new Error(
+                `Could not find the value in DB to return for query: "${sqlStr}" with params: "${params.join()}"`
+            );
         }
     }
 
